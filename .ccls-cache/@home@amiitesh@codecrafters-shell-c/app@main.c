e7d0e41c315455b1334d4a70dcfac31385898471
Hash-Cracker <tspamiitesh@gmail.com>
2024-12-24 20:13:19 +0530

    Implement echo fixes

--- .ccls-cache/@home@amiitesh@codecrafters-shell-c/app@main.c
+++ .ccls-cache/@home@amiitesh@codecrafters-shell-c/app@main.c
@@ -18,10 +18,10 @@ int main() {
   while(1){
     printf("$ ");
     fgets(input, 100, stdin);
-    input[strlen(input) - 1] = '\0';
+    input[strcspn(input, "\n")] = '\0';
 
     if(!strcmp(input, "exit 0")){
-      exit(0);
+      return 0;
     }
     if(strncmp(input, "echo", strlen("echo")) == 0){
       printf("%s\n", input + 5);
